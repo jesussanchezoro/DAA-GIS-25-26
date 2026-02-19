2f0ca2a38a9916385f84f8ca9e9b08b571e6e195
jesussanchezoro <jesus.sanchezoro@urjc.es>
2026-02-19 15:45:56 +0100

    Subidos mochila voraz y planificación

---
+++ 2_voraces/greedy_knapsack.py
@@ -0,0 +1,37 @@
+
+def get_best_item(candidates, v, w):
+    best_item = 0
+    best_ratio = -1
+    for c in candidates:
+        ratio = v[c] / w[c]
+        if ratio > best_ratio:
+            best_item = c
+            best_ratio = ratio
+    return best_item
+
+
+def greedy_knapsack(v, w, W):
+    candidates = set()
+    n = len(v)
+    for i in range(n):
+        candidates.add(i)
+    sol = [0] * n
+    weight = 0
+    while candidates and weight < W:
+        best = get_best_item(candidates, v, w)
+        candidates.remove(best)
+        if w[best] + weight <= W:
+            sol[best] = 1
+            weight += w[best]
+        else:
+            sol[best] = (W-weight) / w[best]
+            weight = W
+    return sol
+
+
+v = [20, 30, 66, 40, 60]
+w = [10, 20, 30, 40, 50]
+W = 100
+
+sol = greedy_knapsack(v, w, W)
+print(sol)
---
+++ 2_voraces/waiting_time.py
@@ -0,0 +1,38 @@
+
+def get_best_task(candidates, tasks):
+    best_task = 0
+    best_task_time = 0x3f3f3f3f
+    for c in candidates:
+        if tasks[c] < best_task_time:
+            best_task = c
+            best_task_time = tasks[c]
+    return best_task
+
+
+def order_tasks(tasks):
+    candidates = set()
+    for i in range(len(tasks)):
+        candidates.add(i)
+    sol = []
+    while candidates:
+        best = get_best_task(candidates, tasks)
+        candidates.remove(best) 
+        sol.append(best)
+    return sol
+
+
+def calculate_waiting_time(sol, tasks):
+    times = []
+    accum = 0
+    for i in range(len(sol)):
+        task = sol[i]
+        accum += tasks[task]
+        times.append(accum)
+    print(times)
+    print(sum(times))
+
+
+tasks = [5, 10, 3]
+sol = order_tasks(tasks)
+print(sol)
+calculate_waiting_time(sol, tasks)
